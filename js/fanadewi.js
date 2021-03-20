96e48bc287aa191e48db7acab79e28e62b26cc67
fanadewi <fitriholly@gmail.com>
2021-03-20 15:03:45 +0700

    send real message

--- js/fanadewi.js
+++ js/fanadewi.js
@@ -5,9 +5,18 @@ $("#submit-button").click(function() {
         $("#submit-button").attr("disabled", false);
         alert("You should enter your message first!");
     } else {
-        // TODO: send real message to me
-        alert("Thanks for the message, it's not really sent yet, but still, thanks! " + $("#message-field").val());
-
+        $.ajax('https://ana-mailer.herokuapp.com/message', {
+            type: 'POST',
+            data: {message: $("#message-field").val()},
+            success: function (result) {
+                console.log(result)
+                alert("Thanks for the message, it's really sent to Ana!");
+            },
+            error: function (err) {
+                console.log(err)
+                alert("Ouch the mailer is down >.< but still, Thanks for the message!");
+                }
+        });
         // reset form
         $("#message-field").val("")
         $("#submit-button").attr("disabled", false);
